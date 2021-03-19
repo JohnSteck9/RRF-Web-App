670c75ad51fd5fc0f2b2dfadbf15282a1e4b6086
JohnSteck9 <m0nitor2048@gmail.com>
2021-03-19 17:05:16 +0200

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {Button, Col, Row} from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<Row>
+  <Col>
+
+  </Col>
+  <Col>
+    <h2>qwerty1</h2>
+    <Button variant="primary">Primary</Button>
+  </Col>
+</Row>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
